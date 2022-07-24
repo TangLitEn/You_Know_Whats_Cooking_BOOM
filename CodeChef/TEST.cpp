e5fea51e8974337b9c8955b8da56acc2ab8eed09
TangLitEn <65808174+TangLitEn@users.noreply.github.com>
2022-07-24 20:13:54 +0800

    Create TEST.cpp

---
+++ CodeChef/TEST.cpp
@@ -0,0 +1,18 @@
+//Problem: TEST Contest: LRNDSA01 User: lien_0212
+
+#include <iostream>
+using namespace std;
+
+int hold;
+
+int main() {
+	do 
+	{
+	    cin>>hold;
+	    if (hold!=42)
+	    {
+	        cout<<hold<<endl;
+	    }
+	}while (hold!=42);
+	return 0;
+}
