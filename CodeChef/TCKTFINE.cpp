4223ed6c3dd603b9a37e580f976db5737f6ccc04
LIEN <65808174+TangLitEn@users.noreply.github.com>
2022-09-01 01:25:19 +0800

    Create TCKTFINE.cpp

---
+++ CodeChef/TCKTFINE.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define db double
+
+using namespace std;
+
+bool toggleBool(bool Bool);
+bool even(ll x);
+ll fact(ll x);
+ll combination(ll n, ll r);
+db arithmeticSum(db a,db n,db d);
+
+void solve(void)
+{
+    ll a,b,c; 
+    cin>>a>>b>>c;
+    ll dif = b-c;
+    cout<<a*dif<<endl;
+}
+
+int main(void)
+{
+    ll n;
+    cin>>n;
+    while(n--) solve();
+    return 0;
+}
+
+bool toggleBool(bool Bool){
+    //toggle the inputed Bool
+    if(Bool)return false;
+    else return true;
+    //return the opposite
+}
+bool even(ll x){
+    return (x%2==0);
+}
+ll fact(ll x) {
+    if(x==0) return 1;
+    else return fact(x-1)*x;
+}
+ll combination(ll n, ll r){
+    return fact(n)/(fact(n-r)*fact(r));
+}
+db arithmeticSum(db a,db n,db d){
+    return (n/2)*(2*a+(n-1)*d);
+}
