e68f636a681ff24c459a1a294529d0c0b72bd62a
LIEN <65808174+TangLitEn@users.noreply.github.com>
2022-09-19 23:44:45 +0800

    Create INDIPERM.cpp

---
+++ CodeChef/INDIPERM.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define db double
+
+using namespace std;
+
+void solve(void)
+{
+    ll N; 
+    cin >> N; // permutation length
+    cout<< N <<" ";
+    for (ll i=1;i<N;i++) cout<<i<<" ";
+    cout<<endl;
+}
+
+int main(void)
+{
+    ll n;
+    cin>>n;
+    while(n--) solve();
+    return 0;
+}
